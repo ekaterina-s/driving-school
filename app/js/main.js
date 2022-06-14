68f742f69fb7d1982f996635511bfd70b999d6d5
ekaterina-s <catshavrina@gmail.com>
2022-06-14 10:31:02 +0200

    написала js к бургеру, первый вариант верстки блока филиалы

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,12 @@
-import {testFunction} from './scripts/test';
+import { testFunction } from './scripts/test';
 testFunction();
+
+const burger = document.querySelector('.js-burger');
+const headerOverlay = document.querySelector('.js-overlay');
+const body = document.querySelector('.js-body');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('_active');
+  headerOverlay.classList.toggle('_active');
+  body.classList.toggle('lock');
+});
