a067f8752f8768e40f9f47e8218eb73e91902b0d
ekaterina-s <catshavrina@gmail.com>
2022-06-17 10:09:02 +0200

    исправила верстку табов в секции филиалы

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,33 @@ burger.addEventListener('click', () => {
   headerOverlay.classList.toggle('_active');
   body.classList.toggle('lock');
 });
+
+const tabButtons = document.querySelectorAll('.js-tab-button');
+const tabcontentLists = document.querySelectorAll('.js-tabcontent-list');
+const tabcontentLinks = document.querySelectorAll('.tabcontent__link');
+
+// tabButtons.forEach((item, index) => {
+//   tabButtons[index].addEventListener('click', () => {
+//     tabButtons[index].className = tabButtons[index].className.replace(' _active', '');
+//   });
+// });
+
+// tabButtons.forEach((item, index) => {
+//   tabButtons[index].addEventListener('click', () => {
+//     if(tabButtons[index].contains.classList('_active')) {
+//       tabButtons[index].remove.classList('_active');
+//     } else {
+//       tabButtons[index].classList.add('_active');
+//     };
+    // tabButtons[index].classList.toggle('_active');
+
+    // tabcontentLists.forEach((item, index) => {
+    //   tabcontentLists[index].classList.toggle('_active');
+    // });
+    // tabcontentLinks.forEach((item, index) => {
+    //   tabcontentLinks[index].addEventListener('click', () => {
+    //     tabcontentLinks[index].classList.toggle('_active');
+    //   })
+    // });
+//   })
+// });
